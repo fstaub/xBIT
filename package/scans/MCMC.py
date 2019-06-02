67dcb4a64b36027a321c16b63c8931d8c50ea124
Florian Staub <florian.staub@googlemail.com>
2019-06-02 13:17:37 +0200

    changed MCMC routine to make it run again

--- package/scans/MCMC.py
+++ package/scans/MCMC.py
@@ -1,4 +1,6 @@
 from package.scanning import Scan as Scan
+import numpy as np
+import os
 
 scan_name = "MCMC"
 
@@ -14,9 +16,9 @@ class NewScan(Scan):
 #  - let several chains run in parallel ( = > how to define number of chains in input?)
 #  - gauss distribution always assumed  = > sufficient?
 
-    def __init__(self, screen, input, main_dir, temp_dir, debug, curses, log):
-        Scan.__init__(self, screen, input, main_dir, temp_dir,
-                      debug, curses, log)
+    def __init__(self, inputs, config):
+        Scan.__init__(self, inputs, config)
+
         self.last_step = []
         self.likelihood_last_step = 0
         self.steps = 1
@@ -24,7 +26,7 @@ class NewScan(Scan):
         self.converged = False
 
         self.variances = []
-        for v in self.variables.values():
+        for v in self.inputs['Variables'].values():
             self.variances.append(v[1])
 
     def get_next_step(self, log):
@@ -34,17 +36,17 @@ class NewScan(Scan):
 
     def get_starting_point(self):
             return [np.random.uniform(x[0][0], x[0][1])
-                    for x in self.variables.values()]
+                    for x in self.inputs['Variables'].values()]
 
     def jumpQ(self, log, guess=-1):
             new_point = self.all_data[-1]
             new_lh = self.likelihood(new_point[1])
-            if new_lh > self.likelihood_last_step or new_lh/self.likelihood_last_step > np.random.uniform():
+            if (new_lh > self.likelihood_last_step) or (new_lh/self.likelihood_last_step > np.random.uniform()):
                 log.info("jumped %i; new_lh: %.2E,  old_lh: %.2E"
                          % (self.steps, new_lh, self.likelihood_last_step))
                 log.info("values of obs: " + str(new_point[1]))
-                if self.curses:
-                    screen.status_mcmc_obs(self.screen, new_point[1])
+                if self.config.cursesQ:
+                    screen.status_mcmc_obs(self.config.screen, new_point[1])
                 self.likelihood_last_step = new_lh
                 self.steps = self.steps + 1
                 self.last_step = self.next_step
@@ -52,54 +54,44 @@ class NewScan(Scan):
                 log.debug("no jump! new_lh: %.2E,  old_lh: %.2E"
                           % (new_lh, self.likelihood_last_step))
                 log.debug("values of obs: " + str(new_point[1]))
-            if self.curses:
-                screen.status_mcmc(self.screen, self.steps, self.points_total,
+            if self.config.cursesQ:
+                screen.status_mcmc(self.config.screen, self.steps, self.points_total,
                                    self.likelihood_last_step, new_lh)
                 if guess > 0:
-                    screen.status_mcmc_guess(self.screen, guess, new_lh)
+                    screen.status_mcmc_guess(self.config.screen, guess, new_lh)
 
-    # Just a simplte criterium as place-holder
-    def check_convergence(self):
-            if self.steps > 1000:
-                self.converged = True
+    # # Just a simplte criterium as place-holder
+    # def check_convergence(self):
+    #         if self.steps > 1000:
+    #             self.converged = True
 
     def run_next_point(self, next, log):
-        self.runner.run_point(self, next, os.path.join(self.temp_dir, "id0"),
-                              self.output_file, log)
-        if self.input_and_observables.empty() is False:
+        self.runner.run(self.config.log, sample=[next])
+        if os.path.exists(self.settings['SPheno']['OutputFile']):
                 self.points_total += 1
-                try:
-                    self.all_data.append(self.input_and_observables.get())
-                except:
-                    log.error("Problem with getting the observables")
 
     def test_next(self, log):
         log.info('MCMC count: %i Total points,  %i Jumps'
                  % (self.points_total, self.steps))
         next = self.get_next_step(log)
         self.run_next_point(next, log)
-        self.jumpQ(log)
-        self.check_convergence()
+        if os.path.exists(self.settings['SPheno']['OutputFile']):
+            self.jumpQ(log)
+        # self.check_convergence()
 
-    def run(self, log):
+    def run(self):
         # start with random point
-        log.info("Starting MCMC")
-        self.start_run(log)
-        if self.curses:
-            screen.start_mcmc(self.screen)
-        while self.input_and_observables.empty():
+        self.config.log.info("Starting MCMC")
+        if self.config.cursesQ:
+            screen.start_mcmc(self.config.screen)
+        while len(self.all_data) == 0:
             start = self.get_starting_point()
-            self.runner.run_point(self, start,
-                                  os.path.join(self.temp_dir, "id0"),
-                                  self.output_file, log)
-        log.info("Found first point in MCMC: %s" % str(start))
-        first_point = self.input_and_observables.get()
-        self.all_data.append(first_point)
+            self.runner.run(self.config.log, sample=[start])
+        self.config.log.info("Found first point in MCMC: %s" % str(start))
+        first_point = self.all_data[0]
         self.likelihood_last_step = self.likelihood(first_point[1])
         self.last_step = first_point[0]
 
         # run the chain
-        while not self.converged:
-            self.test_next(log)
-
-        self.finish_run(log)
+        while (self.points_total < self.inputs['Setup']['Points']):
+            self.test_next(self.config.log)
